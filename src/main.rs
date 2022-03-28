7f1f653abf31f5005bf0ff8644d95454e173d8b4
Gethin Davies <grd.davies@gmail.com>
2022-03-27 19:31:37 -0600

    :bug: Fix commit-msg hook
    commit-msg hook argv is the path to the commit file, not the message
    string itself

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,22 @@ use std::collections::HashMap;
 
 /// A Precommit hook which throws an error if en emoji shortcode is misspelled
 fn main() {
-    let commit_msg = std::env::args().nth(1).expect("No commit message!");
+    let commit_msg_path = std::env::args().nth(1).expect("Commit not found");
+    let commit_msg = std::fs::read_to_string(commit_msg_path).unwrap();
     let re: Regex = Regex::new(r#"(?::)(\S{1,})(?::)"#).unwrap();
-    for x in re.captures_iter(&commit_msg) {
-        if !lookup(x.get(1).unwrap().as_str()) {
+    for x in re
+        .captures_iter(&commit_msg)
+        .map(|x| x.get(1).unwrap().as_str())
+    {
+        if !is_valid_emoji_shortcode(x) {
+            eprintln!("Unknown emoji shortcode: {}", x);
             std::process::exit(1);
         }
     }
 }
 
-fn lookup(text: &str) -> bool {
-    let bytes = include_bytes!("../emojis.json");
+fn is_valid_emoji_shortcode(text: &str) -> bool {
+    let bytes = include_bytes!("emojis.json");
     let emojis: HashMap<String, String> = serde_json::from_slice(bytes).unwrap();
     emojis.contains_key(text)
 }
