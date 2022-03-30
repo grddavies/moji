90b941c4c131f0d5217f7156aa0e9cfcde01f7a5
Gethin Davies <grd.davies@gmail.com>
2022-03-29 23:59:51 -0600

    :house: Use `~/.moji` to store emoji list
    :bug: Also fix annoying CRLF bug
    
    :bug: Also fix regex

--- src/hooks/commit-msg.sh
+++ src/hooks/commit-msg.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-
-commit_msg_path=$1
-if !(test -f emojis.json); then
-    echo "Fetching emoji list from GitHub..."
-    curl -s \
-        -H "Accept: application/vnd.github.v3+json" \
-        https://api.github.com/emojis \
-        -o emojis.json
-fi
-cat $commit_msg_path | grep -oP "(?<=:)(\S{1,})(?=:)" | while read -r code; do
-    if !(grep -q '"$code"' emojis.json); then
-        echo "Invalid emoji shortcode '$code'" 1>&2
-        exit 1
-    fi
+commit_msg_path=$1; MOJI_HOME="$HOME/.moji"; EMOJI_DICT="$MOJI_HOME/emojis.json";
+if !(test -f $EMOJI_DICT); then echo "Fetching emoji list from GitHub..." curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/emojis --create-dirs -o $EMOJI_DICT; fi; cat $commit_msg_path | grep -oP "(?<=:)(\S{1,})(?=:)" | while read -r code; do if !(grep -q \"$code\" $EMOJI_DICT); then echo "Invalid emoji shortcode '$code'" 1>&2; exit 1; fi
 done
