b860b929df73d7c3c560edc7745885fe1f36fea3
Gethin Davies <grd.davies@gmail.com>
2022-05-02 00:33:59 +0100

    :keyboard:	Get a search query from stdin

--- src/search.rs
+++ src/search.rs
@@ -1,10 +1,27 @@
 use crate::PromptStyle;
-use console::Emoji;
+use console::{Key, Term};
 
 pub fn interactive(style: PromptStyle) {
-    eprintln!(
-        "{} Interactive search function not yet implemented {}",
-        style.error.apply_to("[ERROR]:"),
-        Emoji("\u{1F480}", "")
-    )
+    let term = Term::stdout();
+    let mut query = String::new();
+    term.write_str("\u{1F50E} ").unwrap();
+    loop {
+        match term.read_key().unwrap() {
+            Key::Enter => break,
+            Key::Backspace => {
+                query.pop();
+                term.clear_chars(1).unwrap()
+            }
+            Key::Char(c) => {
+                query.push(c);
+                let mut tmp = [0u8; 4];
+                term.write_str(c.encode_utf8(&mut tmp)).unwrap()
+            }
+            _ => (),
+        }
+    }
+    term.clear_line().unwrap();
+    term.write_line(&format!("{}", style.secondary.apply_to("Searched:")))
+        .unwrap();
+    term.write_line(&query).unwrap();
 }
